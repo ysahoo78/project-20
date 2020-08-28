aa81347b3a00cedebeb59a2940970a30f0daef9b
ysahoo78 <70029595+ysahoo78@users.noreply.github.com>
2020-08-28 12:59:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
+//creating all the variables
 var car, wall, speed, weight
 
 
 
 function setup() {
+  //creating the canvas like a rectangle
   createCanvas(1000,400);
+  //speed and weight have to be random
   speed = random(55,90);
   weight = random(400,1500);
+  //creating the wall and car sprites
   car = createSprite(50,200,50,50);
   wall = createSprite(900,200,60,200);
-  
+  //giving velocity and color 
   car.velocityX = speed;
   car.shapeColor = "grey";
 
@@ -16,8 +20,9 @@ function setup() {
 }
 
 function draw() {
+  //making the background black
   background(32,32,32);
-  
+  //changing the color of the car when it touches the wall
   if (collision(car, wall)) { 
     car.velocityX = 0;
     var deformation = (0.5 * weight * speed * speed) / 22500;
@@ -32,5 +37,6 @@ function draw() {
     car.shapeColor = "red";  
     }
   }
+  //making sure all the sprites are displayed
   drawSprites();
 }
